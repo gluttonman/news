abe78dc2c02bea7e296ad6a110f06c09870ae886
gluttonman <lijunboy00@163.com>
2015-07-21 16:52:19 +0800

    查询看房路线信息

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,8 @@
  */
 var express = require("express");
 var mysql = require("mysql");
-var data;
+var article_data;
+var route_data;
 var connection = mysql.createConnection({
     host:"192.168.0.3",
     user:"root",
@@ -12,15 +13,18 @@ var connection = mysql.createConnection({
     port:"3306"
 });
 connection.connect();
-connection.query("select id,title,logofile from zccontent order by addtime desc limit 10",function(err,row,fields){
-   data=row;
+connection.query("select id,title,logofile from zccontent order by addtime desc limit 3",function(err,row,fields){
+    article_data=row;
+});
+connection.query("select id,title from zccontent where status=30 and catalogid=126 ",function(err,row,fields){
+    route_data=row;
 });
 connection.end();
 
 var router = express.Router();
 router.get('/', function (req, res, next) {
-    console.info(data[0]);
-    res.render('login', {articles:data});
+
+    res.render('login', {articles:article_data,routes:route_data});
 });
 
 module.exports = router;
