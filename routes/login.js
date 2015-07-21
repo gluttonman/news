1d2ac82a052a61f8145b891691ce4afe13f04d74
gluttonman <lijunboy00@163.com>
2015-07-21 16:05:59 +0800

    使用mysql读取数据，用jade展示模板信息。初步实现

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,25 @@
  * Created by Administrator on 2015/7/21 0021.
  */
 var express = require("express");
+var mysql = require("mysql");
+var data;
+var connection = mysql.createConnection({
+    host:"192.168.0.3",
+    user:"root",
+    password:"thinkpad",
+    database:"zcms2216451",
+    port:"3306"
+});
+connection.connect();
+connection.query("select id,title,logofile from zccontent order by addtime desc limit 10",function(err,row,fields){
+   data=row;
+});
+connection.end();
+
 var router = express.Router();
 router.get('/', function (req, res, next) {
-    res.render('login', {});
+    console.info(data[0]);
+    res.render('login', {articles:data});
 });
 
 module.exports = router;
