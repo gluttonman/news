5df23571a97ad3829daeb081f9b2d47d5caabda6
gluttonman <lijunboy00@163.com>
2015-07-27 14:11:11 +0800

    finish

--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,9 @@ var express = require("express");
 var dbManager = require("../mymodules/dbmodule/DBManager").DBManager;
 var article_data;
 var route_data;
-dbManager.select("select id,title,logofile from zccontent order by addtime desc limit 3",function(rows){
+dbManager.select("select t.id,t.title,t.logofile,t.staticFileName,t.linkflag,t.redirecturl,l.path from zccontent t inner join zccatalog l on t.catalogid=l.id order by t.addtime desc limit 3",function(rows){
     article_data = rows;
+    console.info(rows);
 });
 dbManager.select("select id,title from zccontent where status=30 and catalogid=126 ",function(rows){
     route_data = rows;
